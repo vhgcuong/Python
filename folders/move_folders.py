e77fa1ed82ed92f05ce4efc59a255217ca285ba8
Cuong Vu Hung (D9) <cuong.vuhung@vti.com.vn>
2024-07-09 10:33:03 +0700

    Move folders Refactory code

--- folders/move_folders.py
+++ folders/move_folders.py
@@ -3,21 +3,23 @@ import shutil
 import argparse
 
 
-def move_folders(src, dic):
-    if os.path.isdir(src) is False or os.path.isdir(dic) is False:
-        raise ("Something wrong")
+def move_folders(src, dst):
+    if not os.path.isdir(src):
+        raise ValueError(f"Source path '{src}' is not a valid directory.")
+    if not os.path.isdir(dst):
+        raise ValueError(f"Destination path '{dst}' is not a valid directory.")
+
     for sub_folder in os.listdir(src):
         old_path = os.path.join(src, sub_folder)
-        shutil.move(old_path, dic)
+        new_path = os.path.join(dst, sub_folder)
+        shutil.move(old_path, new_path)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="""Move all folders in other folders"""
-    )
+    parser = argparse.ArgumentParser(description="Move all folders from the source directory to the destination directory.")
+    parser.add_argument('--src_path', type=str, required=True, help="Source directory path.")
+    parser.add_argument('--dst_path', type=str, required=True, help="Destination directory path.")
 
-    parser.add_argument('--src_path', type=str, required=True)
-    parser.add_argument('--dic_path', type=str, required=True)
     args = parser.parse_args()
 
-    move_folders(args.src_path, args.dic_path)
+    move_folders(args.src_path, args.dst_path)
