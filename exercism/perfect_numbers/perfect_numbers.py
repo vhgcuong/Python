d4e843e2e8cf23271350b9671b77ee36de329591
Yotoko <vhgcuong95@gmail.com>
2024-08-07 10:50:32 +0700

    Perfect number #1

--- exercism/perfect_numbers/perfect_numbers.py
+++ exercism/perfect_numbers/perfect_numbers.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def classify(number):
     """ A perfect number equals the sum of its positive divisors.
 
@@ -7,5 +10,25 @@ def classify(number):
     if number < 1:
         raise ValueError("Classification is only possible for positive integers.")
 
+    if number == 1:
+        return "deficient"
+
+    factors = {1}
+    for i in range(2, int(sqrt(number)) + 1):
+        if number % i == 0:
+            factors.add(i)
+            factors.add(number // i)
+
+    factor_sum = sum(factors)
+
+    match (factor_sum == number, factor_sum < number):
+        case (True, False):
+            return "perfect"
+        case (False, True):
+            return "deficient"
+        case (_, _):
+            return "abundant"
 
 
+if __name__ == '__main__':
+    classify(28)
--- exercism/perfect_numbers/test_perfect_numbers.py
+++ exercism/perfect_numbers/test_perfect_numbers.py
@@ -1,15 +1,16 @@
 from perfect_numbers import classify
 import pytest
 
-
 PERFECT = "perfect"
 DEFICIENT = "deficient"
 ABUNDANT = "abundant"
 
+
 def error():
     raise ValueError("Classification is only possible for positive integers.")
 
-class InvalidInputsTest:
+
+class TestInvalidInputs:
     def test_zero_is_rejected_as_it_is_not_a_positive_integer(self):
         with pytest.raises(ValueError, match=r'Classification is only possible for positive integers.') as excinfo:
             classify(0)
@@ -23,7 +24,7 @@ class InvalidInputsTest:
         assert "Classification is only possible for positive integers." in str(excinfo.value)
 
 
-class PerfectNumbersTest:
+class TestPerfectNumbers:
     def test_smallest_perfect_number(self):
         assert classify(6) == PERFECT
 
@@ -34,7 +35,7 @@ class PerfectNumbersTest:
         assert classify(33550336) == PERFECT
 
 
-class DeficientNumbersTest:
+class TestDeficientNumbers:
     def test_smallest_deficient_number(self):
         assert classify(8) == DEFICIENT
 
@@ -47,11 +48,13 @@ class DeficientNumbersTest:
     def test_edge_case_no_factors_other_than_itself_is_classified_correctly(self):
         assert classify(1) == DEFICIENT
 
+
 class AbundantNumbersTest:
     def test_smallest_abundant_number(self):
         assert classify(12) == ABUNDANT
 
     def test_medium_abundant_number(self):
         assert classify(30) == ABUNDANT
+
     def test_large_abundant_number(self):
         assert classify(33550335) == ABUNDANT
