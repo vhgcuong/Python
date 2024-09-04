fe0ad21bf5780f0f37bff22bc7878244fb5c7ec8
Yotoko <vhgcuong95@gmail.com>
2024-09-04 17:11:07 +0700

    Sulotion 1 ... isogram

--- exercism/isogram/isogram.py
+++ exercism/isogram/isogram.py
@@ -1,2 +1,26 @@
-def is_isogram(string):
-    pass
+from string import ascii_lowercase
+
+ALPHABET = set(ascii_lowercase)
+
+
+def is_isogram(string: str):
+    dic = {}
+
+    for item in string.lower():
+        if item in dic.keys():
+            dic[item] += 1
+        else:
+            dic[item] = 1
+
+    for (key, value) in dic.items():
+        if key in [' ', '-', '_']:
+            continue
+        if str(key).lower() not in ALPHABET:
+            return False
+        if value != 1:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    is_isogram('alphAbet')
