2012d97151a5d268501a5eaa09dfac49aea489ba
Yotoko <vhgcuong95@gmail.com>
2024-09-04 17:21:06 +0700

    Solution 2 isogram

--- exercism/isogram/isogram.py
+++ exercism/isogram/isogram.py
@@ -4,20 +4,9 @@ ALPHABET = set(ascii_lowercase)
 
 
 def is_isogram(string: str):
-    dic = {}
-
-    for item in string.lower():
-        if item in dic.keys():
-            dic[item] += 1
-        else:
-            dic[item] = 1
-
-    for (key, value) in dic.items():
-        if key in [' ', '-', '_']:
-            continue
-        if str(key).lower() not in ALPHABET:
-            return False
-        if value != 1:
+    lst = list(filter(lambda char: char in ALPHABET, set(string.lower())))
+    for item in lst:
+        if (string.lower()).count(item) > 1:
             return False
     return True
 
