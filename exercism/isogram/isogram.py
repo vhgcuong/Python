70fa1908711e61e13e1f39a8d7cbce461f2e6d41
Yotoko <vhgcuong95@gmail.com>
2024-09-05 10:24:31 +0700

    Update isogram solution

--- exercism/isogram/isogram.py
+++ exercism/isogram/isogram.py
@@ -1,5 +1,11 @@
+# import re
+
+
 def is_isogram(phrase):
     scrubbed = phrase.replace('-', '').replace(' ', '').lower()
+    # scrubbed = [ltr.lower() for ltr in phrase if ltr.isalpha()]
+    # scrubbed = "".join(re.findall("[a-zA-Z]", phrase)).lower()
+
     return len(scrubbed) == len(set(scrubbed))
 
 
