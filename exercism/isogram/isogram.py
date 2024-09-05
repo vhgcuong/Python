fd00630c3c85219d82d9d159d51e3364c2e709a8
Yotoko <vhgcuong95@gmail.com>
2024-09-05 10:22:50 +0700

    Update isogram solution

--- exercism/isogram/isogram.py
+++ exercism/isogram/isogram.py
@@ -1,15 +1,6 @@
-from string import ascii_lowercase
-
-ALPHABET = set(ascii_lowercase)
-
-
-def is_isogram(string: str):
-    str_convert = string.lower()
-    lst = list(filter(lambda char: char in ALPHABET, set(str_convert)))
-    for item in lst:
-        if (str_convert).count(item) > 1:
-            return False
-    return True
+def is_isogram(phrase):
+    scrubbed = phrase.replace('-', '').replace(' ', '').lower()
+    return len(scrubbed) == len(set(scrubbed))
 
 
 if __name__ == '__main__':
